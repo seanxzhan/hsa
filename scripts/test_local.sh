6ccd8b6094f0d510f00d49ca862be81fdaf46e30
Sean Zhan <seanzhan0319@hotmail.com>
2024-04-01 23:47:48 -0400

    ran local_2, good results with dangling networks

--- scripts/test_local.sh
+++ scripts/test_local.sh
@@ -10,3 +10,19 @@ python run/local/local_$expt.py --test --it $it --ti 0 --mask --rt --parts 2
 python run/local/local_$expt.py --test --it $it --ti 0 --mask --ro --parts 0
 python run/local/local_$expt.py --test --it $it --ti 0 --mask --ro --parts 1
 python run/local/local_$expt.py --test --it $it --ti 0 --mask --ro --parts 2
+
+python run/local/local_$expt.py --test --it $it --ti 1
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --rt --parts 0
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --rt --parts 1
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --rt --parts 2
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 0
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 1
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 2
+
+python run/local/local_$expt.py --test --it $it --ti 2
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --rt --parts 0
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --rt --parts 1
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --rt --parts 2
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 0
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 1
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 2
