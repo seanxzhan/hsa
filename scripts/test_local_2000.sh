3fb1b88918ddd67bc0354d3f73e86561ec20274f
Sean Zhan <seanzhan0319@hotmail.com>
2024-04-09 10:23:14 -0400

    ran local_19 learning just arms, not that great

--- scripts/test_local_2000.sh
+++ scripts/test_local_2000.sh
@@ -58,18 +58,29 @@ it=$2
 # python run/local/local_$expt.py --test --it $it --ti 19
 # python run/local/local_$expt.py --test --it $it --ti 20
 
-python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 11 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 12 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 13 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 14 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 15 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 16 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 17 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 18 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 19 --mask --ro --parts 12
-python run/local/local_$expt.py --test --it $it --ti 20 --mask --ro --parts 12
+# python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 11 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 12 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 13 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 14 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 15 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 16 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 17 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 18 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 19 --mask --ro --parts 4
+# python run/local/local_$expt.py --test --it $it --ti 20 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 21 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 22 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 23 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 24 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 25 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 26 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 27 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 28 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 29 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 30 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 31 --mask --ro --parts 4
--- scripts/test_local_2000_further_merge.sh
+++ scripts/test_local_2000_further_merge.sh
@@ -16,17 +16,17 @@ python run/local/local_$expt.py --test --it $it --ti 1
 # python run/local/local_$expt.py --test --it $it --ti 1 --mask --rt --parts 3
 # python run/local/local_$expt.py --test --it $it --ti 1 --mask --rt --parts 12
 # python run/local/local_$expt.py --test --it $it --ti 1 --mask --rt --parts 15
-# python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 3
-# python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 12
-# python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 15
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 3
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 12
+python run/local/local_$expt.py --test --it $it --ti 1 --mask --ro --parts 15
 
 python run/local/local_$expt.py --test --it $it --ti 2
 # python run/local/local_$expt.py --test --it $it --ti 2 --mask --rt --parts 3
 # python run/local/local_$expt.py --test --it $it --ti 2 --mask --rt --parts 15
 # python run/local/local_$expt.py --test --it $it --ti 2 --mask --rt --parts 16
-# python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 3
-# python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 15
-# python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 16
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 3
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 15
+python run/local/local_$expt.py --test --it $it --ti 2 --mask --ro --parts 16
 
 python run/local/local_$expt.py --test --it $it --ti 3
 # python run/local/local_$expt.py --test --it $it --ti 3 --mask --rt --parts 2
@@ -34,27 +34,27 @@ python run/local/local_$expt.py --test --it $it --ti 3
 # python run/local/local_$expt.py --test --it $it --ti 3 --mask --rt --parts 4
 # python run/local/local_$expt.py --test --it $it --ti 3 --mask --rt --parts 12
 # python run/local/local_$expt.py --test --it $it --ti 3 --mask --rt --parts 15
-# python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 2
-# python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 3
-# python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 4
-# python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 12
-# python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 15
+python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 2
+python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 3
+python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 4
+python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 12
+python run/local/local_$expt.py --test --it $it --ti 3 --mask --ro --parts 15
 
 python run/local/local_$expt.py --test --it $it --ti 9
 # python run/local/local_$expt.py --test --it $it --ti 9 --mask --rt --parts 3
 # python run/local/local_$expt.py --test --it $it --ti 9 --mask --rt --parts 12
 # python run/local/local_$expt.py --test --it $it --ti 9 --mask --rt --parts 15
-# python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 3
-# python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 12
-# python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 15
+python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 3
+python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 12
+python run/local/local_$expt.py --test --it $it --ti 9 --mask --ro --parts 15
 
 python run/local/local_$expt.py --test --it $it --ti 10
 # python run/local/local_$expt.py --test --it $it --ti 10 --mask --rt --parts 3
 # python run/local/local_$expt.py --test --it $it --ti 10 --mask --rt --parts 12
 # python run/local/local_$expt.py --test --it $it --ti 10 --mask --rt --parts 15
-# python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 3
-# python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 12
-# python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 15
+python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 3
+python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 12
+python run/local/local_$expt.py --test --it $it --ti 10 --mask --ro --parts 15
 
 python run/local/local_$expt.py --test --it $it --ti 11
 python run/local/local_$expt.py --test --it $it --ti 12
