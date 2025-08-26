e593767e7ca348b2b1f1bf8c6ff5f529d4b43d3d
Sean Zhan <seanzhan0319@hotmail.com>
2025-08-26 17:30:56 -0400

    Revert "more expts"
    This reverts commit ba2076f9fdb26a75c792584f81cdc22c187164e0.

--- utils/polyvis.py
+++ utils/polyvis.py
@@ -17,17 +17,17 @@ def vis_sdf(sdf, dims, img_path, plot_hist=False, hist_path=None):
     scalar_vals = np.reshape(sdf, dims)
     # print(np.min(scalar_vals), np.max(scalar_vals))
 
-    # if plot_hist:
-    #     import matplotlib.pyplot as plt
-    #     from utils import misc
-    #     plt.hist(sdf.flatten(), bins=50, color='blue')
-    #     plt.title("Histogram of SDF Values")
-    #     plt.xlabel("SDF Value")
-    #     plt.ylabel("Frequency")
-    #     plt.xlim(-1.5, 1.5)
-    #     misc.save_fig(plt, '', hist_path)
-    #     plt.close()
-    #     # plt.show()
+    if plot_hist:
+        import matplotlib.pyplot as plt
+        from utils import misc
+        plt.hist(sdf.flatten(), bins=50, color='blue')
+        plt.title("Histogram of SDF Values")
+        plt.xlabel("SDF Value")
+        plt.ylabel("Frequency")
+        plt.xlim(-1.5, 1.5)
+        misc.save_fig(plt, '', hist_path)
+        plt.close()
+        # plt.show()
 
     ps_plane = ps.add_scene_slice_plane()
     ps_plane.set_draw_plane(False)
@@ -43,8 +43,7 @@ def vis_sdf(sdf, dims, img_path, plot_hist=False, hist_path=None):
                                 enabled=True,
                                 cmap='rainbow')
     
-    # ps.screenshot(filename=img_path, transparent_bg=False)
-    ps.show()
+    ps.screenshot(filename=img_path, transparent_bg=False)
     ps.remove_all_structures()
     ps.remove_last_scene_slice_plane()
 
@@ -90,8 +89,8 @@ def vis_occ(occ, dims, img_path, plot_hist=False, hist_path=None):
                                 enabled=True,
                                 cmap='rainbow')
     
-    # ps.screenshot(filename=img_path, transparent_bg=False)
-    ps.show()
+    ps.screenshot(filename=img_path, transparent_bg=False)
+    # ps.show()
     # ps.remove_volume_grid("sdf grid")
     ps.remove_all_structures()
     ps.remove_last_scene_slice_plane()
@@ -103,8 +102,7 @@ def vis_mesh(vertices, faces, img_path):
     ps.set_ground_plane_mode("shadow_only")
 
     ps.register_surface_mesh("mesh", vertices, faces)
-    ps.show()
-    # ps.screenshot(filename=img_path, transparent_bg=False)
+    ps.screenshot(filename=img_path, transparent_bg=False)
     ps.remove_all_structures()
 
 
@@ -126,31 +124,31 @@ if __name__ == "__main__":
         'results/occflexi/occflexi_1/img-1000sdf.png',
         plot_hist=True,
         hist_path='results/occflexi/occflexi_1/hist-1000sdf.png')
-    # mesh = trimesh.load_mesh('results/occflexi/occflexi_1/1000fleximesh.obj')
-    # V, F = mesh.vertices, mesh.faces
-    # vis_mesh(
-    #     V, F,
-    #     'results/occflexi/occflexi_1/img-1000fleximesh.png')
+    mesh = trimesh.load_mesh('results/occflexi/occflexi_1/1000fleximesh.obj')
+    V, F = mesh.vertices, mesh.faces
+    vis_mesh(
+        V, F,
+        'results/occflexi/occflexi_1/img-1000fleximesh.png')
 
-    # vis_occ(
-    #     np.load('results/occflexi/occflexi_1/outocc.npy'),
-    #     (64, 64, 64),
-    #     'results/occflexi/occflexi_1/img-outocc.png',
-    #     plot_hist=True,
-    #     hist_path='results/occflexi/occflexi_1/hist-outocc.png')
-    # mesh = trimesh.load_mesh('results/occflexi/occflexi_1/outoccmesh.obj')
-    # V, F = mesh.vertices, mesh.faces
-    # vis_mesh(
-    #     V, F,
-    #     'results/occflexi/occflexi_1/img-outoccmesh.png')
-    # vis_sdf(
-    #     np.load('results/occflexi/occflexi_1/outsdf.npy'),
-    #     (32, 32, 32),
-    #     'results/occflexi/occflexi_1/img-outsdf.png',
-    #     plot_hist=True,
-    #     hist_path='results/occflexi/occflexi_1/hist-outsdf.png')
-    # mesh = trimesh.load_mesh('results/occflexi/occflexi_1/outfleximesh.obj')
-    # V, F = mesh.vertices, mesh.faces
-    # vis_mesh(
-    #     V, F,
-    #     'results/occflexi/occflexi_1/img-outfleximesh.png')
+    vis_occ(
+        np.load('results/occflexi/occflexi_1/outocc.npy'),
+        (64, 64, 64),
+        'results/occflexi/occflexi_1/img-outocc.png',
+        plot_hist=True,
+        hist_path='results/occflexi/occflexi_1/hist-outocc.png')
+    mesh = trimesh.load_mesh('results/occflexi/occflexi_1/outoccmesh.obj')
+    V, F = mesh.vertices, mesh.faces
+    vis_mesh(
+        V, F,
+        'results/occflexi/occflexi_1/img-outoccmesh.png')
+    vis_sdf(
+        np.load('results/occflexi/occflexi_1/outsdf.npy'),
+        (32, 32, 32),
+        'results/occflexi/occflexi_1/img-outsdf.png',
+        plot_hist=True,
+        hist_path='results/occflexi/occflexi_1/hist-outsdf.png')
+    mesh = trimesh.load_mesh('results/occflexi/occflexi_1/outfleximesh.obj')
+    V, F = mesh.vertices, mesh.faces
+    vis_mesh(
+        V, F,
+        'results/occflexi/occflexi_1/img-outfleximesh.png')
